7265b0f1f577a34163fdc7bc8d13a4ab3fa4ec9b
Aleksan_09 <platondovgan2006@gmail.com>
2024-02-12 19:02:34 +0300

    uuuun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -31,6 +31,21 @@ def promotion_image():
 @app.route('/image_mars')
 def image_mars():
     return render_template('index.html')
+
+@app.route('/astronaut_selection', methods=['GET', 'POST'])
+def astronaut_selection():
+    if request.method == 'GET':
+        return render_template('astronaut_selection.html')
+    elif request.method == 'POST':
+        print(request.form['surname'])
+        print(request.form['name'])
+        print(request.form['mail'])
+        print(request.form['educ'])
+        print(request.form['prof'])
+        print(request.form['gender'])
+        print(request.form['motiv'])
+        print(request.form['stay'])
+    return render_template('astronaut_selection.html')
 
 
 if __name__ == '__main__':
