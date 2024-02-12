7bdd68073b1247ad75365d967fd015048ea5a834
Aleksan_09 <platondovgan2006@gmail.com>
2024-02-12 18:11:31 +0300

    uuuu

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ def promotion():
     <br>Присоединяйся!'''
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return render_template('promotion_image.html')
+
+
 @app.route('/image_mars')
 def image_mars():
     return render_template('index.html')
